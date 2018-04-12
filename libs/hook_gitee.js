72bf8a456f5e2c6e0db08b538fa437152b96619a
EdisonTang <15921328654@163.com>
2018-04-12 20:16:20 +0800

    Finish: run specific file according to repository name

--- libs/hook_gitee.js
+++ libs/hook_gitee.js
@@ -19,7 +19,8 @@ class Hook {
 
     this.isRunning = true
 
-    const shell = exec('sh gitee_worker.sh', {
+    const execCmd = this.getCmd(params)
+    const shell = exec(execCmd, {
       cwd: CWD
     }, async (error, stdout, stderr) => {
       if (error) {
@@ -41,6 +42,23 @@ class Hook {
 
   setToNext(cb) {
     this.nextCallback = cb
+  }
+
+  // set exec command according to repository name temporarily
+  // may change as demand changes
+  getCmd(params) {
+    let command = ''
+    switch (params.repository.name) {
+      case 'uwa-answer-next-version':
+        command = 'sh gitee_worker.sh'
+        break
+      case 'uwa-main-site-next-version':
+        command = 'sh mainsite_autodeploy.sh'
+        break
+      default: 
+        break
+    }
+    return command
   }
 
 }
